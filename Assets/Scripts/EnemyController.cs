c94936e9c84675783b769016d0fe22f6bdbc035e
cmprog <2270827+cmprog@users.noreply.github.com>
2019-07-21 16:08:51 -0500

    added basic enemy concept

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(GameObjectTags.Player))
+        {
+            var lPollenCollectable = collision.GetComponent<PollenCollectable>();
+            lPollenCollectable.CurrentPollen *= 0.5;
+
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/FollowMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public sealed class FollowMovement : MonoBehaviour
+{
+    public GameObject target;
+
+    public float speed;
+    public float turn;
+
+    public float minTurnThreshold;
+
+    public float actualTurn;
+    public float targetAngleDeg;
+    public float targetAngleDegOffset;
+
+
+    private void FixedUpdate()
+    {
+        var lTargetOffset = this.target.transform.position - this.transform.localPosition;
+        var lTargetAngleRad = Mathf.Atan2(lTargetOffset.y, lTargetOffset.x);
+        var lTargetAngleDeg = (lTargetAngleRad * Mathf.Rad2Deg) - 90;
+
+        var lOffsetDeg = Mathf.DeltaAngle(this.transform.rotation.eulerAngles.z, lTargetAngleDeg);
+        
+        this.actualTurn = 0;
+        this.targetAngleDeg = lTargetAngleDeg;
+        this.targetAngleDegOffset = lOffsetDeg;
+
+        if (Mathf.Abs(lOffsetDeg) >= this.minTurnThreshold)
+        {
+            var lActualTurn = this.turn;
+            if (lOffsetDeg < 0)
+            {
+                lActualTurn = -this.turn;
+            }
+
+            this.actualTurn = lActualTurn;
+
+            this.transform.Rotate(0, 0, lActualTurn);
+        }
+
+        var lOrientationDeg = this.transform.rotation.eulerAngles.z + 90;
+        var lOrientationRad = Mathf.Deg2Rad * lOrientationDeg;
+        var lOrientationX = Mathf.Cos(lOrientationRad);
+        var lOrientationY = Mathf.Sin(lOrientationRad);
+                
+        this.transform.Translate(lOrientationX * this.speed, lOrientationY * this.speed, 0, Space.World);        
+    }
+}
